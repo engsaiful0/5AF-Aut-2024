921c326110839469299c385ceba5dcdba67a7d36
saiful islam <engsaiful0@gmail.com>
2024-10-30 09:46:38 +0600

    php and mysql

---
+++ class9_php_mysql/data_save.php
@@ -0,0 +1,16 @@
+<?php
+
+$connection=mysqli_connect("localhost",username: "root","","5af_aut_2024");
+//echo '<pre>';
+//print_r($connection);
+//print_r($_POST);
+$studentName=$_POST['studentName'];
+$roll=$_POST['roll'];
+$mobile=$_POST['mobile'];
+
+mysqli_query($connection,"INSERT INTO students (studentName,roll,mobile) values('$studentName','$roll','$mobile')");
+
+//echo $studentName;
+
+
+?>
