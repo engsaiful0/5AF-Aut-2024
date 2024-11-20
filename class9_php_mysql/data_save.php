66be71f6ce4beb2188d1d5c6dc49f0f04e9a898d
saiful islam <engsaiful0@gmail.com>
2024-11-20 08:47:02 +0600

    last change

--- class9_php_mysql/data_save.php
+++ class9_php_mysql/data_save.php
@@ -1,16 +1,13 @@
 <?php
 
-$connection=mysqli_connect("localhost",username: "root","","5af_aut_2024");
+$connection = mysqli_connect("localhost", username: "root", "", "5af_aut_2024");
 //echo '<pre>';
 //print_r($connection);
 //print_r($_POST);
-$studentName=$_POST['studentName'];
-$roll=$_POST['roll'];
-$mobile=$_POST['mobile'];
+$studentName = $_POST['studentName'];
+$roll = $_POST['roll'];
+$mobile = $_POST['mobile'];
 
-mysqli_query($connection,"INSERT INTO students (studentName,roll,mobile) values('$studentName','$roll','$mobile')");
+mysqli_query($connection, "INSERT INTO students (studentName,roll,mobile) values('$studentName','$roll','$mobile')");
 
 //echo $studentName;
-
-
-?>
